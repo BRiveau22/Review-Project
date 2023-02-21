ebad82ec51911ede271d1baf63260eb9086b8b4b
connorstubbs <connor_stubbs@uri.edu>
2023-02-21 07:10:20 -0500

    Display functions

--- Main.cpp
+++ Main.cpp
@@ -5,10 +5,10 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-	std::string file_name = argv[1];
-
+	//std::string file_name = argv[1];
+    std::string file_name = "/Users/connorstubbs/Desktop/URI Freshman/CSC 212/Projects/Review Project/Review-Project/Gradebook_Test.txt";
 	Gradebook current_gradebook(file_name);
-
+    current_gradebook.Read_Grades();
 	std::cout << "Welcome to your virtual gradebook!\nWhat would you like to do today?" << std::endl;
 	current_gradebook.Generate_Home_UI();
 }
