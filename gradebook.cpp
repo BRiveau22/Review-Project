f8aa22605ae77f464429578b1f791b680970d1cb
BRiveau <briveau@uri.edu>
2023-02-23 15:37:25 -0500

    Updated Edit_Grades function to make sure that it returns to UI.

--- gradebook.cpp
+++ gradebook.cpp
@@ -185,6 +185,7 @@ void Gradebook::Edit_Grade(int index) {
     std::cout << "\nwhat course is this assignment from?" << std::endl;
     std::cin >> input;
     courses_vector[index] = input;
+	Generate_Action2_UI();
 }
 
 void Gradebook::Del_Grade(int index) {
